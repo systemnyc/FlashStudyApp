53a788db54b08cc850632dce75a87e33f21649c2
Daryl Bosco <darylsbosco@gmail.com>
2026-06-23 14:51:38 -0400

    Updates

--- app/screens/AddCardScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function AddCardScreen() {
-  return (
-    <View style={{ padding: 20 }}>
-      <Text style={{ fontSize: 22 }}>Add Card Screen (coming soon)</Text>
-    </View>
-  );
-}
--- app/types/navigation.ts
+++
@@ -1,7 +0,0 @@
-export type RootStackParamList = {
-  Home: undefined;
-  RandomCard: undefined;
-  Categories: undefined;
-  CategoryDetail: { category: string };
-  AddCard: undefined;
-};
